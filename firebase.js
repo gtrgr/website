a0beb6046db62f8312113d0c64965b0538544373
gtrgr <163936850+gtrgr@users.noreply.github.com>
2026-05-25 20:54:26 -0500

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyCGVnwdOmx1hw3hlUqTLToYVlXckYymnS0",
+  authDomain: "winger-training-app.firebaseapp.com",
+  projectId: "winger-training-app",
+  storageBucket: "winger-training-app.firebasestorage.app",
+  messagingSenderId: "1083020465037",
+  appId: "1:1083020465037:web:7c8f5ebf3fdc2afbaaa3c4"
 };
