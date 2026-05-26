851ed7845c0b8e1e51ebedcd7d4a4903c4ea973a
gtrgr <163936850+gtrgr@users.noreply.github.com>
2026-05-25 20:43:59 -0500

    firebase.js
    Removed Firebase initialization and configuration code.

--- firebase.js
+++ firebase.js
@@ -6,19 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "...",
   appId: "..."
 };
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
